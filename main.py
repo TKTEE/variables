70a7e4e388b8cc260c31142e882b7bf776da37b6
eMobilis <tonykaranja15@gmail.com>
2023-02-21 14:53:45 +0300

    my variables

---
+++ main.py
@@ -0,0 +1,6 @@
+name = "Tony"
+age = 22
+country = "Kenya"
+print(f"My name is {name} and I am {age} years")
+print(f"I am from {country}")
+print("")
---
+++ var.py
@@ -0,0 +1,4 @@
+num1 = input("Enter number: ")
+num2 = input("Enter number: ")
+result = float(num1) + float(num2)
+print(result)
