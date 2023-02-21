dd09ae106234c077fc5c662fa24210185a240303
eMobilis <tonykaranja15@gmail.com>
2023-02-21 15:16:17 +0300

    first commit

---
+++ data_types.py
@@ -0,0 +1,13 @@
+#Integer
+
+age = 20
+#string
+
+city = "Kiambu"
+#float
+
+temperature = 36.5
+#Booleans
+
+score = True
+alive = True
---
+++ exercise.py
@@ -0,0 +1,4 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+result = float(num1) + float(num2)
+print(result)
